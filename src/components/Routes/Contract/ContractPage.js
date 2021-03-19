18341a0440272f5487df343e607ee769f3bbaaa6
Phil Li <qft.gtr@gmail.com>
2021-03-19 13:03:26 +0800

    Update ContractPage.js

--- src/components/Routes/Contract/ContractPage.js
+++ src/components/Routes/Contract/ContractPage.js
@@ -7,14 +7,20 @@ export default class PlatonContractPage extends ContractPage {
     if (!abiData) {
       return
     }
-    if (abiData.vmType) {
-      console.log(abiData.vmType)
-      return { abi: [], vmType: abiData.vmType }
-    }
     try {
       abiData.abi = JSON.parse(abiData.abi)
     } catch {
       throw new Error('Invalid ABI structure.')
+    }
+    if (abiData.vmType) {
+      abiData.abi = abiData.abi.map(item => {
+        return {
+          ...item,
+          inputs: item.input,
+          type: item.type === 'Action' ? 'function' : '',
+          stateMutability: item.constant ? 'view' : ''
+        }
+      })
     }
     return abiData
   }
